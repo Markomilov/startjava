41fce8b46b6c3b97d89ff259960dba569caaaf3e
Markomilov <markomilov.ivan@gmail.com>
2020-02-06 18:34:38 +0300

    implement HW_1

---
+++ Lesson_1/Variable.java
@@ -0,0 +1,20 @@
+public class Variable {
+	public static void main(String[] args){
+		byte core = 2;
+		short speed = 1600;
+		int memory = 4 + 4;
+		long graphics = 1536;
+		float memorys = 2.30F;
+		double osversion = 10.153;
+		char z = 'z';
+		boolean yes = true;
+		System.out.println(core);
+		System.out.println(speed);
+		System.out.println(memory);
+		System.out.println(graphics);
+		System.out.println(osversion);
+		System.out.println(memorys);
+		System.out.println(z);
+		System.out.println(yes);
+	}
+}
