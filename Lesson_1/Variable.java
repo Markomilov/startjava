95828a27167b9bbb7d746f601fd4d8266180b6aa
IvMarkomilov <markomilov.ivan@gmail.com>
2020-02-10 08:58:58 +0300

    FirstHWEdited

--- Lesson_1/Variable.java
+++ Lesson_1/Variable.java
@@ -1,21 +1,21 @@
 public class Variable {
-	public static void main(String[] args){
+	public static void main(String[] args) {
 		byte core = 2;
 		short speed = 1600;
 		int memory = 4 + 4;
 		long graphics = 1536;
 		float memorys = 2.30F;
-		double osversion = 10.153;
-		char z = 'z';
-		boolean yes = true;
+		double OsVersion = 10.153;
+		char ModelGraphics = 'h';
+		boolean OsCatalina = true;
+		
 		System.out.println(core);
 		System.out.println(speed);
 		System.out.println(memory);
 		System.out.println(graphics);
-		System.out.println(osversion);
+		System.out.println(OsVersion);
 		System.out.println(memorys);
-		System.out.println(z);
-		System.out.println(yes);
-		
+		System.out.println(ModelGraphics);
+		System.out.println(OsCatalina);
 	}
 }
